bebf04ddcdb0901dc01b3eb3b50a281fdf898880
chandrakant nyaupane <chandra@MacBook-Pro.local>
2021-06-14 20:18:59 +0300

    final darkmode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+
+import { Content } from './components/Content';
+import { useDarkMode } from './styles/useDarkMode';
+import { Toggle } from './components/Toggle';
+import { GlobalStyles, lightTheme, darkTheme } from './styles/globalStyles';
+
+
+
+const Container = styled.div`
+ max-width: 50%;
+ margin: 8rem auto 0;
+`;
 
 function App() {
+  const [theme, toggleTheme] = useDarkMode();
+ 
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeMode}>
+    <Container>
+    <GlobalStyles />
+    <Toggle theme={theme} toggleTheme={toggleTheme}/>
+      <Content />
+    </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Content = () => {
+    return (
+        <div>
+           <h1>This is how you change theme from dark mode to light mode and vice-versa.</h1> 
+           <button className="btn-primary">Explore</button>
+        </div>
+    )
+}
